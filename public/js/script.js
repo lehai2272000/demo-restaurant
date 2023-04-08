0c1e7b97fda7b82055e5c3d4d12ce1b119220a4c
unknown <you@example.com>
2023-04-08 16:13:25 +0700

    add reponsize

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,17 @@ menuBar.addEventListener("click", function(){
     menuBar.classList.toggle("active");
     document.querySelector(".menu-items").classList.toggle("active");
 })
+
+const menuTitle = document.querySelector(".menu-title");
+menuTitle.addEventListener("click", function(x){
+    if(x.target.classList.contains("menu-button"))
+    {
+        const Target = x.target.getAttribute("data-title")
+        menuTitle.querySelector(".active").classList.remove("active");
+        x.target.classList.add("active");
+
+        const menuItem = document.querySelector(".menu");
+        menuItem.querySelector(".menu-item-content.active").classList.remove("active");
+        menuItem.querySelector(Target).classList.add("active");
+    }
+})
